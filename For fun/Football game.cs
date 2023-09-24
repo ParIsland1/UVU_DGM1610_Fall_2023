3aee06253dd95de7c9e20634f2c74babc7e5529a
Aidan Adams <118240533+ParIsland1@users.noreply.github.com>
2023-09-24 15:00:03 -0600

    added team select

--- For fun/Football game.cs
+++ For fun/Football game.cs
@@ -4,20 +4,42 @@ public class Program
 {
 	public static void Main()
 	{
-		Random random = new Random();
-		int min = 50;
-		int max = 99;
-		int number = random.Next(min, max + 1);
-		Console.WriteLine("The kick goes " + number + " yards!");
+		GameStart();
 		
+		Kickoff();
 		Return();
+		
+		
 	}
 	public static void Return()
 	{
 		Random random = new Random();
 		int min = 25;
-		int max = 50;
+		int max = 99;
 		int number = random.Next(min, max + 1);
 		Console.WriteLine("They return " + number + " yards!");
+	}
+	public static void Kickoff()
+	{
+		Random random = new Random();
+		int min = 50;
+		int max = 99;
+		int kickoff = random.Next(min, max + 1);
+		Console.WriteLine("The kick goes " + kickoff + " yards!");
+	}
+	public static void GameStart()
+	{
+		string[] leagueTeams = {"SD Sharks", "NY Bulls", "MIA Suns", "DEN Stallions", "LV Outlaws"};
+		
+		Console.WriteLine("Welcome to First Down!");
+		Console.WriteLine("Select your team coach.");
+		Console.WriteLine();
+		
+		foreach (var item in leagueTeams) 
+		{
+			Console.WriteLine(item);
+		}
+		string playerTeam = Console.ReadLine();
+		Console.WriteLine("You are the " + playerTeam + " head coach!");	
 	}
 }
